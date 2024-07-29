81c55da937776c72f115c37547a24d34ab16de47
MiniMandrake <78477643+MiniMandrake@users.noreply.github.com>
2024-07-29 10:11:53 -0500

    functioning API
    implemented API token to convertor

--- bionicReadingConvertor.py
+++ bionicReadingConvertor.py
@@ -1,23 +1,17 @@
-# print("Hello World")
 
 import requests
 
 url = "https://bionic-reading1.p.rapidapi.com/convert"
 
-payload = {
-	"content": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-	"response_type": "html",
-	"request_type": "html",
-	"fixation": "1",
-	"saccade": "10"
-}
+payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"response_type\"\r\n\r\nhtml\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"request_type\"\r\n\r\nhtml\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"fixation\"\r\n\r\n1\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"saccade\"\r\n\r\n10\r\n-----011000010111000001101001--\r\n\r\n"
 headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": "1a5a7bc8cemshd00b7071346980cp1d4334jsn2b7195570453",
-	"X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
+	"x-rapidapi-key": "1a5a7bc8cemshd00b7071346980cp1d4334jsn2b7195570453",
+	"x-rapidapi-host": "bionic-reading1.p.rapidapi.com",
+	"Content-Type": "multipart/form-data; boundary=---011000010111000001101001"
 }
 
 response = requests.post(url, data=payload, headers=headers)
 
-print(response.json())
+print(response.status_code)
 
+print(response.content)
