5ff39059c056c6baf1f9157a916bf57506d6d71b
MiniMandrake <78477643+MiniMandrake@users.noreply.github.com>
2024-08-13 01:00:14 -0500

    implemented .env file to hide API keys
    wrote API key in .env file
    import .env file and token name
    program maintains function

--- bionicReadingConvertor.py
+++ bionicReadingConvertor.py
@@ -1,5 +1,7 @@
-
+import os
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 # Bionic Reader API accessed through Rapid API
 # default params
@@ -7,7 +9,7 @@ url = "https://bionic-reading1.p.rapidapi.com/convert"
 
 payload = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"response_type\"\r\n\r\nhtml\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"request_type\"\r\n\r\nhtml\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"fixation\"\r\n\r\n1\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"saccade\"\r\n\r\n10\r\n-----011000010111000001101001--\r\n\r\n"
 headers = {
-	"x-rapidapi-key": "1a5a7bc8cemshd00b7071346980cp1d4334jsn2b7195570453",
+	"x-rapidapi-key": os.getenv("BionicReadingToken"),
 	"x-rapidapi-host": "bionic-reading1.p.rapidapi.com",
 	"Content-Type": "multipart/form-data; boundary=---011000010111000001101001"
 }
