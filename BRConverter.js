38bc0bda34e379b032f333567f97e5e363c59e8c
Zhi <zhiyangteh@gmail.com>
2024-10-14 19:33:59 -0500

    working input to on site text output

--- BRConverter.js
+++ BRConverter.js
@@ -1,32 +1,57 @@
-require("dotenv").config();
-dotenv.config();
+async function getPayload(payload) {
+  const url = "https://bionic-reading1.p.rapidapi.com/convert";
+  const data = new FormData();
 
-const url = "https://bionic-reading1.p.rapidapi.com/convert";
-const data = new FormData();
+  data.append("content", payload);
+  data.append("response_type", "html");
+  data.append("request_type", "html");
+  data.append("fixation", "1");
+  data.append("saccade", "10");
 
-data.append(
-  "content",
-  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-);
-data.append("response_type", "html");
-data.append("request_type", "html");
-data.append("fixation", "1");
-data.append("saccade", "10");
+  const options = {
+    method: "POST",
+    headers: {
+      "x-rapidapi-key": "1a5a7bc8cemshd00b7071346980cp1d4334jsn2b7195570453",
+      "x-rapidapi-host": "bionic-reading1.p.rapidapi.com",
+    },
+    body: data,
+  };
 
-const options = {
-  method: "POST",
-  headers: {
-    "x-rapidapi-key": BionicReadingToken,
-    "x-rapidapi-host": "bionic-reading1.p.rapidapi.com",
-  },
-  body: data,
-};
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+    document.getElementById("output").innerHTML = result;
+  } catch (error) {
+    console.error(error);
+  }
 }
-return result;
+
+function readFile() {
+  const fileInput = document.getElementById("input");
+  const out = document.getElementById("output");
+
+  fileInput.addEventListener("change", (event) => {
+    const files = event.target.files;
+
+    if (files.length > 0) {
+      const file = files[0];
+
+      // Do something with the file, e.g., read its contents:
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const fileContent = e.target.result;
+        console.log(fileContent);
+        getPayload(fileContent);
+      };
+      console.log("bruh");
+      reader.readAsText(file);
+    }
+  });
+}
+
+readFile();
+
+// getPayload("gello");
+// getPayload(text);
